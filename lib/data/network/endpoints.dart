d8964e1bd21eaa73a85f604ed32163cc37b61fda
vipul bansal <vipulbansalkkp@gmail.com>
2024-05-14 21:56:27 +0530

    set up

---
+++ lib/data/network/endpoints.dart
@@ -0,0 +1,5 @@
+class Endpoints {
+  static const countries = 'countries';
+  static const states = 'states';
+
+}
---
+++ lib/wrappers/dismiss_keyboard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DismissKeyboardWrapper extends StatelessWidget {
+  final Widget child;
+
+  const DismissKeyboardWrapper({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus &&
+            currentFocus.focusedChild != null) {
+          FocusManager.instance.primaryFocus!.unfocus();
+        }
+      },
+      child: child,
+    );
+  }
+}
