35dddafdabb47f30a443bfed04f86bc81b3112fa
vipul bansal <vipulbansalkkp@gmail.com>
2024-05-15 14:17:24 +0530

    done

--- lib/services/route_service.dart
+++ lib/services/route_service.dart
@@ -1,13 +1,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 import 'package:vipul_test_assignment/presentation/screens/home_screen.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'root');
-final GlobalKey<NavigatorState> _userHomeNavigatorKey =
-    GlobalKey<NavigatorState>(debugLabel: 'home');
+
 
 
 final GoRouter router = GoRouter(
